c02671a436c6fe4993754fac7d5642b79b44a860
James Barnes <james@testtrends.co.uk>
2020-10-27 07:38:39 +0000

    Unit tested the browser helper class.

---
+++ DFC.TestAutomation.UI.UnitTests/HelperTests/HelperLibraryTests/BrowserHelperTests.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="BrowserHelperTests.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.TestAutomation.UI.Helper;
+using System;
+using Xunit;
+
+namespace DFC.TestAutomation.UI.UnitTests.HelperTests.HelperLibraryTests
+{
+    public class BrowserHelperTests
+    {
+        [Fact]
+        public void UnrecognisedBrowserName()
+        {
+            Assert.Throws<InvalidOperationException>(() => new BrowserHelper("unrecognised-browser-name"));
+        }
+
+        [Theory]
+        [InlineData("chrome")]
+        [InlineData(" chrome ")]
+        [InlineData("cHroMe")]
+        public void RecognisedBrowserName(string browserName)
+        {
+            _ = new BrowserHelper(browserName);
+        }
+
+        [Theory]
+        [InlineData("chrome", BrowserType.Chrome)]
+        [InlineData("browserstack", BrowserType.BrowserStack)]
+        [InlineData("firefox", BrowserType.Firefox)]
+        [InlineData("ie", BrowserType.InternetExplorer)]
+        public void GetBrowserType(string browserName, BrowserType browserType)
+        {
+            var browserHelper = new BrowserHelper(browserName);
+            Assert.Equal(browserType, browserHelper.GetBrowserType());
+        }
+
+        [Theory]
+        [InlineData("browserstack", true)]
+        [InlineData("chrome", false)]
+        [InlineData("firefox", false)]
+        [InlineData("ie", false)]
+        public void IsExecutingInBrowserStack(string browserName, bool isExecutingInBrowserStack)
+        {
+            var browserHelper = new BrowserHelper(browserName);
+            Assert.Equal(isExecutingInBrowserStack, browserHelper.IsExecutingInBrowserStack());
+        }
+    }
+}
--- DFC.TestAutomation.UI/Helper/BrowserHelper.cs
+++ DFC.TestAutomation.UI/Helper/BrowserHelper.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using Io.Cucumber.Messages;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
